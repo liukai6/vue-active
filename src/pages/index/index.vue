c5a737800ed3572ce100e59a4d929f2fabe39973
liukai6 <13283815737@163.com>
2018-09-30 13:59:14 +0800

    弹窗

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    nihao
+    <mt-button @click="btn" type="default">default</mt-button>
+  </div>
+</template>
+<script>
+import { Toast } from 'mint-ui'
+export default {
+  name: 'Index',
+  data () {
+    return {}
+  },
+  methods: {
+    btn () {
+      Toast({
+        message: 'Upload Complete',
+        iconClass: 'mint-toast-icon mintui mintui-field-warning',
+        duration: 5000
+      })
+    }
+  }
+}
+</script>
+<style>
+  .mintui-field-warning {
+    font-size: 30px;
+  }
+</style>
